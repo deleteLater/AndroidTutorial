ab8a9c19ab7d98b100249b4668af272057e435d8
Zhang TongXie <mikcczhang@qq.com>
2019-05-19 10:54:56 +0800

    upload from local

--- FragmentBestPractice/app/src/main/java/com/zhang/fragmentbestpractice/NewsTitleFragment.java
+++ FragmentBestPractice/app/src/main/java/com/zhang/fragmentbestpractice/NewsTitleFragment.java
@@ -1,6 +1,121 @@
 package com.zhang.fragmentbestpractice;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPane;
+
+    private List<News> getNews() {
+        List<News> newsList = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {
+            News news = new News();
+            news.setTitle("This is news title " + i);
+            news.setContent(getRandomLengthContent("This is news content " + i + ". "));
+            newsList.add(news);
+        }
+        return newsList;
+    }
+
+    private String getRandomLengthContent(String content) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(content);
+        }
+        return builder.toString();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        RecyclerView newsTitleRecyclerView = view.findViewById(R.id.news_title_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        newsTitleRecyclerView.setLayoutManager(layoutManager);
+        NewsAdapter adapter = new NewsAdapter(getNews());
+        newsTitleRecyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_fragment) != null) {
+            isTwoPane = true;
+        } else {
+            isTwoPane = false;
+        }
+    }
+
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+        private List<News> mNewsList;
+
+        public NewsAdapter(List<News> newsList) {
+            mNewsList = newsList;
+        }
+
+        @NonNull
+        @Override
+        public NewsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.news_item,
+                    viewGroup, false);
+            final ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if (isTwoPane) { // 双页模式
+                        NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager()
+                                .findFragmentById(R.id.news_content_fragment);
+                        newsContentFragment.refresh(news.getTitle(), news.getContent());
+                    } else { // 单页模式
+                        NewsContentActivity.actionStart(getActivity(),
+                                news.getTitle(), news.getContent());
+                    }
+                }
+            });
+
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NewsAdapter.ViewHolder viewHolder, int position) {
+            News news = mNewsList.get(position);
+            viewHolder.newsTitleText.setText(news.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView newsTitleText;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                newsTitleText = itemView.findViewById(R.id.news_title);
+            }
+        }
+    }
+
 }
